53ccc7b3b6fc7d7058a06d2e08b254fcc0aacd55
Seagyn Davis <seagyn@flickerleap.com>
2018-10-23 23:50:51 +0200

    Get base into VC and prep layouts

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img class="logo" alt="Vue logo" src="../assets/wordcamp-logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+  .logo {
+    max-width: 200px;
+  }
+</style>
